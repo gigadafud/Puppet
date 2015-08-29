aff7f73c3315a4a82bb4300d2f5ddd908ed91678
gigadafud <stephen@bawks.com>
2015-08-29 07:22:35 -0400

    adding more classes

--- setup-iis.pp
+++ setup-iis.pp
@@ -1,32 +1,83 @@
-windowsfeature { 'IIS':
+
+
+node 'nodename' {
+  include install-telnet
+  include setup-base-iis
+  include remove-default-site
+  include create-sites-folders
+  include testsite
+}
+
+
+exec { 'puppet-fstab':
+    path => 'C:\\Program Files\\Puppet Labs\\Puppet\\bin',
+    command => 'puppet module install puppetlabs-windows',
+}
+
+
+# Class: setup-base-iis
+#
+#
+class setup-base-iis {
+  # resources
+  windowsfeature { 'IIS':
   ensure => present,
   feature_name => [
     'Web-Server',
     'Web-WebServer',
+    'Web-Http-Errors',
+    'Web-Http-Redirect',
+    'Web-Health',
+    'Web-Http-Logging',
+    'Web-Custom-Logging',
+    'Web-Log-Libraries',
+    'Web-ODBC-Logging',
+    'Web-Request-Monitor',
+    'Web-Http-Tracing',
+    'Web-Performance',
+    'Web-Stat-Compression',
+    'Web-Dyn-Compression',
+    'Web-Filtering',
+    'Web-Basic-Auth',
+    'Web-Windows-Auth',
     'Web-Asp-Net45',
+    'Web-AppInit',
     'Web-ISAPI-Ext',
     'Web-ISAPI-Filter',
     'NET-Framework-45-ASPNET',
     'WAS-NET-Environment',
-    'Web-Http-Redirect',
-    'Web-Filtering'
-    'Web-Windows-Auth',
-    'Web-Basic-Auth',
-    'Web-Custom-Logging',
-    'Web-Http-Tracing',
     'Web-Includes'
   ]
+  }
 }
 
-iis::manage_site { 'Default Web Site':
+
+# Class: remove-default-site
+#
+#
+class remove-default-site {
+  # resources
+  iis::manage_site { 'Default Web Site':
   ensure => 'purged',
   app_pool => 'DefaultAppPool'
+  }
 }
 
 
-node 'nodename' {
-  include 'testsite'
+
+# Class: install-telnet
+# 
+#
+class install-telnet {
+  # resources
+  windowsfeature { 'TELNET':
+  ensure => present,
+  feature_name => 'telnet-client'
+  }
+
 }
+
+
 
 class testsite {
   iis::manage_app_pool {'testsiteapppool':
@@ -40,4 +91,22 @@ class testsite {
     host_header => 'testsite',
     app_pool => 'testsiteapppool'
   } 
+}
+
+
+
+
+# Class: create-sites-folders
+#
+#
+class create-sites-folders {
+  # resources
+  # or you can assign them to a variable and use them in the resource
+  $whisper_dirs = [ "c:/sites/", "c:/sites/big",
+                  "c:/sites/missioncontrol",
+                ]
+
+  file { $whisper_dirs:
+    ensure => "directory",
+    }
 }
